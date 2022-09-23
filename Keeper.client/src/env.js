79e04b598bf14ac04c415f9477c70fb93656e360
JeradeaSimmons <hippeejd@gmail.com>
2022-09-23 10:13:23 -0600

    env and development.json

--- Keeper.client/src/env.js
+++ Keeper.client/src/env.js
@@ -1,6 +1,8 @@
 export const dev = window.location.origin.includes('localhost')
-export const baseURL = dev ? 'http://localhost:3000' : ''
+export const baseURL = dev ? 'https://localhost:5001' : ''
 export const useSockets = false
-export const domain = ''
-export const clientId = ''
-export const audience = ''
+
+export const domain = 'hippeejd.us.auth0.com'
+export const clientId = 'sC7bCqbQi9X5OKrtZJ5k9v8NWL6WpLHB'
+export const audience = 'https://mycodeworks.com'
+
